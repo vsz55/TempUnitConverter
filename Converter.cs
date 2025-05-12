9e26a7c4547ea7ea4b77dda7a97d1adb90a63003
vsz55 <vargaszoltan@freemail.hu>
2025-05-12 14:26:03 +0200

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempUnitConverter
+{
+    public class Converter
+    {
+        /*  Celsius-fok átváltás Fahrenheitbe: ℉ = ((℃*9)/5)+32
+          Fahrenheit átváltás Celsius-fokba: ℃=((℉-32)*5)/9
+          Celsius-fok átváltás Kelvinbe: K=℃+273,15
+          Kelvin átváltás Celsius-fokba: ℃=K-273,15
+          Fahrenheit átváltás Kelvinbe: K=((℉+459,67)*5)/9
+          Kelvin átváltás Fahrenheitbe: ℉=((K*9)/5)-459,67
+        */
+        public double CToK(double c) { return c + 273.15; }
+
+        public double CToF(double c) { return ((c * 9) / 5) + 32; }
+
+        public double KToC(double k) { return k - 273.15; }
+
+        public double KToF(double k) { return ((k * 9) / 5) - 459.67; }
+
+        public double FToC(double f) { return ((f - 32) * 5) / 9; }
+
+        public double FToK(double f) { return ((f + 459.67) * 5) / 9; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempUnitConverter
+{
+    class Program
+    {
+        /*
+         0 °C = 32 F = 273.15 °K
+        10 °C = 50 F = 283.15 °K
+        100 °C = 212 F = 373.15 °K
+        0 °K = -273.15 °C = -459.67 F
+        100 °K = -173.15 °C = -279.67 F
+        -273.15 °K = 0 °C = 32 F
+         */
+        static void Main(string[] args)
+        {
+            double c = 10;
+            double k = 0;
+            double f = 32;
+            Converter converter = new Converter();
+            double kk = converter.CToK(c);
+            Console.WriteLine("10 °C = " + kk + " °K");
+            double ff = converter.CToF(c);
+            Console.WriteLine("10 °C = " + ff + " °F");
+            double cc = converter.KToC(k);
+            Console.WriteLine("0 °K = " + cc + " °C");
+            double fff = converter.KToF(k);
+            Console.WriteLine("0 °K = " + fff + " °F");
+            double kkk = converter.FToK(f);
+            Console.WriteLine("32 °F = " + kkk + " °K");
+            double ccc = converter.FToC(f);
+            Console.WriteLine("32 °F = " + ccc + " °C");
+            Console.ReadKey();
+        }
+    }
+}
